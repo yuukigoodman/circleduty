742376671e0922cd2251bf6c1cea4e616b8b29f3
Yuuki Tannai <yuuki.tannai@gmail.com>
2016-12-14 20:31:20 +0900

    go-pagerduty sample

--- src/github.com/yuukigoodman/circleduty/main.go
+++ src/github.com/yuukigoodman/circleduty/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/PagerDuty/go-pagerduty"
 	"github.com/spf13/viper"
 	"github.com/urfave/cli"
 )
@@ -13,11 +14,23 @@ func main() {
 	viper.BindEnv("auth_token")
 	viper.AutomaticEnv()
 
+	authtoken := viper.GetString("auth_token")
+	var opts pagerduty.ListEscalationPoliciesOptions
+	pd := pagerduty.NewClient(authtoken)
+
 	app := cli.NewApp()
 	app.Name = "circleduty"
 	app.Usage = "circleduty fail"
 	app.Action = func(c *cli.Context) error {
-		message := fmt.Sprintf("PagerDuty Incident created: %s", viper.Get("auth_token"))
+		if eps, err := pd.ListEscalationPolicies(opts); err != nil {
+			panic(err)
+		} else {
+			for _, p := range eps.EscalationPolicies {
+				fmt.Println(p.Name)
+			}
+		}
+
+		message := fmt.Sprintf("PagerDuty Incident created: %s", authtoken)
 		fmt.Println(message)
 		return nil
 	}
